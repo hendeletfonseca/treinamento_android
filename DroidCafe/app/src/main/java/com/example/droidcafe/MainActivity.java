c8c8c79996f8e4fc0b53e6fda21dfe25b263f6f8
Hendel Fonseca <hendeletfonseca@gmail.com>
2023-09-17 17:55:58 -0300

    tmp

--- DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
+++ DroidCafe/app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -1,30 +1,66 @@
 package com.example.droidcafe;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
-    private static String mOrderMessage;
+    private static String mOrderMessage = "Shopping car is empty";
     public static final String EXTRA_MESSAGE =
             "com.example.android.droidcafe.extra.MESSAGE";
-    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        actionBar = getSupportActionBar();
-        if (actionBar != null)
-            actionBar.hide();
-    }
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.action_order) {
+            Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+            startActivity(intent);
+            return true;
+        } else if (itemId == R.id.action_status) {
+            displayToast(getString(R.string.action_status_message));
+        } else if (itemId == R.id.action_favorites) {
+            displayToast(getString(R.string.action_favorites_message));
+        } else if (itemId == R.id.action_contact) {
+            displayToast(getString(R.string.action_contact_message));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
